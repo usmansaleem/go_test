aa62f2116d13d4f938c432cab735a41a2bda95be
Usman Saleem <usman@usmans.info>
2020-06-01 12:41:19 +1000

    Exploring Go - Initial commit
    Signed-off-by: Usman Saleem <usman@usmans.info>

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt" 
+	"math/rand"
+	"math"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func add2(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// name returned values
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
+	fmt.Println(add(3,2))
+	fmt.Println(add2(3,2))
+	fmt.Println(swap("x", "y"))
+	fmt.Println(split(17))
+}
