aac240a51816423bcad836ff1465c2ca84e43cdb
Usman Saleem <usman@usmans.info>
2020-06-03 11:48:10 +1000

    variable and types
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- hello.go
+++ hello.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt" 
-	"math/rand"
+	"fmt"
 	"math"
+	"math/rand"
 )
 
 func add(x int, y int) int {
@@ -25,12 +25,41 @@ func split(sum int) (x, y int) {
 	return
 }
 
+// variables
+// var c, python, java bool
+
+// var with initialization
+var j, k int = 1, 2
+
+// var with initialization without type
+var c, python, java = true, false, "yes"
+
+const NAME = "Uzi"
+
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 	fmt.Println(math.Pi)
-	fmt.Println(add(3,2))
-	fmt.Println(add2(3,2))
+	fmt.Println(add(3, 2))
+	fmt.Println(add2(3, 2))
 	fmt.Println(swap("x", "y"))
 	fmt.Println(split(17))
+
+	// variables
+	var i int
+	fmt.Println(i, j, k, c, python, java)
+
+	// short variable declaration
+	m := 10
+	fmt.Println(m)
+
+	// type conversion
+	amount := 10.15
+	amountWhole := uint(amount)
+	fmt.Println(amount, amountWhole)
+
+	// type
+	fmt.Printf("amount type: %T\n amountWhole Type: %T\n", amount, amountWhole)
+
+	fmt.Println("Name: ", NAME)
 }
